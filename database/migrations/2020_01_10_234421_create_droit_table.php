7984be084642e18c3c9a96b938a5b23ae991de1c
Mathias Hanna <mathias.hanna3@gmail.com>
2020-01-11 01:07:27 +0100

    [#12] Création des migrations

---
+++ database/migrations/2020_01_10_234421_create_droit_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDroitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('droit', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('id_utilisateur');
+            $table->integer('id_projet');
+            $table->enum('role', array('1', '2'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('droit');
+    }
+}
