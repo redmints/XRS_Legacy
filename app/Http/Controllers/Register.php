e594c994ad3878de35188e0ea6a2428045b6d5ca
Mathias Hanna <mathias.hanna3@gmail.com>
2020-01-31 18:21:01 +0100

    Déclaration de la variable utilisateur en début de fonction

--- app/Http/Controllers/Register.php
+++ app/Http/Controllers/Register.php
@@ -34,6 +34,7 @@ class Register extends Controller
         $cle = Cle::where('valeur', $cle)->first();
         //On essaie également de récupérer l'email donné
         $email_verification = Utilisateur::where('email', $email)->first();
+        $utilisateur = new Utilisateur;
         //Si l'email n'est pas encore enregistré
         if(!isset($email_verification))
         {
