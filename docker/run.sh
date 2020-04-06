51494813a734b03908798dbe355b8bf4cd41545f
Mathias Hanna <mathias.hanna3@gmail.com>
2020-04-07 01:19:09 +0200

    Vérification qu'un conteneur n'est pas déjà lancé

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --detach -it -p $2:22/tcp $1
+if [[ "$(docker ps -q -f ancestor=$1 2> /dev/null)" == "" ]]; then
+        docker run --detach -it -p $2:22/tcp $1
+else
+        echo "déjà lancé"
+fi
